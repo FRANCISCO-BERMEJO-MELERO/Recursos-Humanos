488e336b950e034b7c8d843451ad905678c76371
Fran <franbermejo.dam@gmail.com>
2024-08-29 12:39:24 +0200

    Mapeamos y listamos los usuarios

--- rh/src/main/java/gm/rh/controlador/EmpleadoControlador.java
+++ rh/src/main/java/gm/rh/controlador/EmpleadoControlador.java
@@ -1,12 +1,16 @@
 package gm.rh.controlador;
 
+import gm.rh.modelo.Empleado;
 import gm.rh.servicio.IEmpleadoServicio;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 //http://localhost:8080/rh-app/
@@ -20,4 +24,11 @@ public class EmpleadoControlador {
     @Autowired
     private IEmpleadoServicio empleadoServicio;
 
+    //http://localhost:8080/rh-app/empleados
+    @GetMapping("/empleados")
+    public List<Empleado> obtenerEmpleados(){
+        var empleados = empleadoServicio.listarEmpleados();
+        empleados.forEach((empleado -> logger.info(empleado.toString())));
+        return empleados;
+    }
 }
