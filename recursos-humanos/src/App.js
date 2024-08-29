ddf6d56c6d41d35192b4066facefb365be40f7eb
Fran <franbermejo.dam@gmail.com>
2024-08-29 21:21:11 +0200

    Creamos y limpiamos el pryecto de react

---
+++ recursos-humanos/src/App.js
@@ -0,0 +1,11 @@
+import "./index.css";
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-red-600 text-4xl ">Sistema de Recursos Humanos</h1>
+    </div>
+  );
+}
+
+export default App;
