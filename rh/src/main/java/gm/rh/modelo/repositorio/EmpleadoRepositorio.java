5ba26cce9c66f7550429f11bc3840fe3c0a716ff
Fran <franbermejo.dam@gmail.com>
2024-09-19 11:55:46 +0200

    Añado docker

---
+++ rh/src/main/java/gm/rh/modelo/repositorio/EmpleadoRepositorio.java
@@ -0,0 +1,9 @@
+package gm.rh.modelo.repositorio;
+
+
+import gm.rh.modelo.Empleado;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmpleadoRepositorio extends JpaRepository<Empleado, Integer> {
+
+}
